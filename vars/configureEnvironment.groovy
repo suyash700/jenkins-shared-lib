99b25dd79b9cd3ce6f83ea91670ae65e40ed3153
Md. Afzal Hassan Ehsani <136967348+iemafzalhassan@users.noreply.github.com>
2025-03-28 01:48:02 +0530

    Update configureEnvironment.groovy

--- vars/configureEnvironment.groovy
+++ vars/configureEnvironment.groovy
@@ -3,10 +3,10 @@
 def call() {
     echo "Starting environment configuration with Ansible"
     
-    withCredentials([
-        string(credentialsId: 'aws-access-key', variable: 'AWS_ACCESS_KEY_ID'),
-        string(credentialsId: 'aws-secret-key', variable: 'AWS_SECRET_ACCESS_KEY')
-    ]) {
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', 
+                      credentialsId: 'aws-credentials', 
+                      accessKeyVariable: 'AWS_ACCESS_KEY_ID', 
+                      secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
         dir('ansible') {
             try {
                 // Install required Ansible collections
