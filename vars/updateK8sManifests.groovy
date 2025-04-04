2d79b7bfac3e9e51ef92a57fc00eae4e1cb52b16
Md Afzal Hassan <iemafzal@Mds-Mac-mini.local>
2025-04-04 17:23:03 +0530

    chores: updated shared lib

---
+++ vars/updateK8sManifests.groovy
@@ -0,0 +1,54 @@
+#!/usr/bin/env groovy
+
+/**
+ * Update Kubernetes manifests with new image tags
+ *
+ * @param imageTag The new image tag to use
+ * @param manifestsPath Path to the Kubernetes manifests directory
+ * @param gitCredentials The ID of the Git credentials
+ * @param gitUserName Git user name for commits
+ * @param gitUserEmail Git user email for commits
+ */
+def call(Map config = [:]) {
+    def imageTag = config.imageTag ?: error("Image tag is required")
+    def manifestsPath = config.manifestsPath ?: 'kubernetes'
+    def gitCredentials = config.gitCredentials ?: 'github-credentials'
+    def gitUserName = config.gitUserName ?: 'Jenkins CI'
+    def gitUserEmail = config.gitUserEmail ?: 'jenkins@example.com'
+    
+    echo "Updating Kubernetes manifests with image tag: ${imageTag}"
+    
+    withCredentials([usernamePassword(
+        credentialsId: gitCredentials,
+        usernameVariable: 'GIT_USERNAME',
+        passwordVariable: 'GIT_PASSWORD'
+    )]) {
+        // Configure Git
+        sh """
+            git config user.name "${gitUserName}"
+            git config user.email "${gitUserEmail}"
+        """
+        
+        // Update deployment manifests with new image tags
+        sh """
+            # Update main application deployment - note the correct image name is iemafzal/easyshop-app
+            sed -i '' 's|image: iemafzal/easyshop-app:.*|image: iemafzal/easyshop-app:${imageTag}|g' ${manifestsPath}/08-easyshop-deployment.yaml
+            
+            # Update migration job
+            sed -i '' 's|image: iemafzal/easyshop-migration:.*|image: iemafzal/easyshop-migration:${imageTag}|g' ${manifestsPath}/12-migration-job.yaml
+            
+            # Check for changes
+            if git diff --quiet; then
+                echo "No changes to commit"
+            else
+                # Commit and push changes
+                git add ${manifestsPath}/*.yaml
+                git commit -m "Update image tags to ${imageTag} [ci skip]"
+                
+                # Set up credentials for push
+                git remote set-url origin https://\${GIT_USERNAME}:\${GIT_PASSWORD}@github.com/iemafzalhassan/EasyShop-KIND.git
+                git push origin HEAD:\${GIT_BRANCH}
+            fi
+        """
+    }
+}
