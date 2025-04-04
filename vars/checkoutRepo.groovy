6cae3659ced0ee818c8f70800cd34f2eb79ee776
Md Afzal Hassan <iemafzal@Mds-Mac-mini.local>
2025-04-04 12:30:14 +0530

    chores: updated shared lib

--- vars/checkoutRepo.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-
-/**
- * Checkout the repository
- */
-def call() {
-    echo "Checking out repository..."
-    checkout scm
-}
--- vars/cleanupWorkspace.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-
-/**
- * Clean up the workspace before starting the build
- */
-def call() {
-    echo "Cleaning up workspace..."
-    cleanWs()
-}
